2114f30a97d8841ed8fc2ed8b685964ca39bc606
João Magalhães <1181053@isep.ipp.pt>
2025-01-06 21:01:18 +0000

    Added quick clicker setting

--- settings.py
+++ settings.py
@@ -4,6 +4,16 @@ import json
 settings = {}
 
 
+def check_for_quick_clicker():
+    def set_setting(setting, value):
+        settings[setting] = value
+
+    if get_setting("quick_clicker"):
+        set_setting("click_cross_enabled", False)
+        set_setting("click_crown_duration", 0.01)
+        set_setting("sleep_time", 0.0)
+
+
 def load_settings(file_path="settings.json"):
     """
     Load settings from a JSON file and store them in the global settings variable.
@@ -12,6 +22,7 @@ def load_settings(file_path="settings.json"):
     try:
         with open(file_path, "r") as file:
             settings = json.load(file)
+            check_for_quick_clicker()
     except FileNotFoundError:
         print(f"Settings file '{file_path}' not found. Using default settings.")
         settings = {}
